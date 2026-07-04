8479d28489ce8437aee693e2de1d9e9d1844e7db
aniketganorkar03 <aniketganorkar03@gmail.com>
2026-07-04 17:39:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  //Add your code here
+  const priceCells = document.querySelectorAll('.prices');
+
+  let total = 0;
+  priceCells.forEach(cell => {
+    total += parseFloat(cell.textContent);
+  });
+
+  const table = document.querySelector('table');
+  const newRow = document.createElement('tr');
+  const totalCell = document.createElement('td');
+  totalCell.textContent = "Total: " + total;
+  totalCell.setAttribute('colspan', '2');
+
+  newRow.appendChild(totalCell);
+  table.appendChild(newRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
